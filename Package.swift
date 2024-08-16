698e3aba2ba67d1e32fac4c52368a6e629034008
杨光 <yangguang@webull.com>
2024-08-16 16:09:45 +0800

    fix: bump version

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", .upToNextMajor(from: "0.17.0")),
-        .package(url: "https://github.com/sunimp/WWCryptoKit.Swift.git", .upToNextMajor(from: "1.3.2"))
+        .package(url: "https://github.com/sunimp/WWCryptoKit.Swift.git", .upToNextMajor(from: "1.3.3"))
     ],
     targets: [
         .target(
@@ -23,7 +22,6 @@ let package = Package(
             dependencies: [
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
-                .product(name: "secp256k1", package: "secp256k1.swift"),
                 .product(name: "WWCryptoKit", package: "WWCryptoKit.Swift"),
             ]),
         .testTarget(
